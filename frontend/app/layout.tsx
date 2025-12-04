106e7896e4ebba4f232b64586f1e2c35de80b506
Stepehn Alarcón <slj212885@gmail.com>
2025-12-04 18:17:20 -0500

    fix(config): configure amplify.yml

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -11,6 +11,9 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: 'La Receta Secreta - Helados y Postres Artesanales',
   description: 'Los mejores helados y postres artesanales',
+  icons: {
+    icon: '/favicon.svg',
+  },
 };
 
 export default function RootLayout({
